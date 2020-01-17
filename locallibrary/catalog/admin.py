506038fd64719e5a4a9d174e76af4704e01b380a
aspiringguru <bmatthewtaylor@gmail.com>
2020-01-18 07:43:51 +1000

    part4, pre adding ModelAdmin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+#app 'catalog' has model.py file
+#classes defined in model.py = Author, Genre, Book, BookInstance
+
+#register these classes
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
